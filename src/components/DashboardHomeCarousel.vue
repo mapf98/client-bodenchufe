fddd436c62adbe228eccc9d5afcd843532fd13bc
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2020-05-25 21:55:58 -0400

    Se eliminó una foto del carusel

--- src/components/DashboardHomeCarousel.vue
+++ src/components/DashboardHomeCarousel.vue
@@ -18,7 +18,6 @@ import Component from "vue-class-component";
 })
 export default class DashboardHomeCarousel extends Vue {
   items = [
-    "https://firebasestorage.googleapis.com/v0/b/bodenchufe-client.appspot.com/o/images%2Faplication%2Fslide1.jpg?alt=media&token=d9a7394c-6fa2-41e5-9554-4d23e748ce31",
     "https://firebasestorage.googleapis.com/v0/b/bodenchufe-client.appspot.com/o/images%2Faplication%2Fslide2.jpg?alt=media&token=5ebe6565-db3a-436a-9ce0-d34a7672fc74",
     "https://firebasestorage.googleapis.com/v0/b/bodenchufe-client.appspot.com/o/images%2Faplication%2Fslide3.jpg?alt=media&token=89e3e1df-6543-4156-ae55-d1c8cbeaa718",
   ];
